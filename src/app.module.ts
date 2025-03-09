00fbb6f3f473884ce56a5abb6baafe46cea3c8b4
Francisco Javier Barrena <fjbarrena@gmail.com>
2025-03-09 22:07:43 +0100

    fix: removed unused services and controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './modules/users/users.module';
 import { CryptoassetsModule } from './modules/cryptoassets/cryptoassets.module';
 import { SecurityModule } from './modules/security/security.module';
 import { ConfigModule } from '@nestjs/config';
-import { User } from './modules/users/entities/user.entity';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
-import { Order } from './modules/cryptoassets/entities/order.entity';
 import AppDataSource from './typeorm.config';
 
 @Module({
@@ -30,9 +26,8 @@ import AppDataSource from './typeorm.config';
     CryptoassetsModule,
     SecurityModule,
   ],
-  controllers: [AppController],
+  controllers: [],
   providers: [
-    AppService,
     {
       provide: APP_GUARD,
       useClass: ThrottlerGuard
