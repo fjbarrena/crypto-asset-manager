4cf1bbb2aa0a69789adb4dd30a29d38424b757d7
Francisco Javier Barrena <fjbarrena@gmail.com>
2025-03-11 00:44:25 +0100

    chore: lint

--- src/decorators/jwt-token.decorator.ts
+++ src/decorators/jwt-token.decorator.ts
@@ -2,5 +2,6 @@ import { createParamDecorator } from '@nestjs/common';
 import { JwtTokenResponse } from 'src/modules/security/dtos/jwt-token.response';
 
 export const JwtToken = createParamDecorator((data, req) => {
+  // eslint-disable-next-line
   return req.switchToHttp().getRequest().user as JwtTokenResponse;
 });
