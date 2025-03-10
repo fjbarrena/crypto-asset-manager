f707e44ca6499c671a5c9314af96f522960f2772
Francisco Javier Barrena <fjbarrena@gmail.com>
2025-03-11 00:48:06 +0100

    fix: added back endpoint

--- src/modules/cryptoassets/cryptoassets.controller.ts
+++ src/modules/cryptoassets/cryptoassets.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import {
   ApiBearerAuth,
   ApiCookieAuth,
@@ -7,17 +7,18 @@ import {
   ApiTags,
 } from '@nestjs/swagger';
 import { CoingeckoService } from './coingecko.service';
+import { Coins } from 'src/model/coins.enum';
 import { OrderService } from './order.service';
 import { CreateOrderRequest } from './dto/create_order.request';
 import { isSuccess } from 'src/model/result.model';
 import { OrderResponse } from './dto/order.response';
+import { CoinsPriceResponse } from './dto/coins_price.response';
 import { JwtGuard } from '../security/guards/jwt.guard';
 import { JwtToken } from 'src/decorators/jwt-token.decorator';
 import { JwtTokenResponse } from '../security/dtos/jwt-token.response';
 
 @ApiTags('cryptoassets')
 @ApiBearerAuth()
-@ApiCookieAuth('crypto-cookie')
 @Controller('cryptoassets')
 @UseGuards(JwtGuard)
 export class CryptoassetsController {
@@ -26,27 +27,50 @@ export class CryptoassetsController {
     private readonly orderService: OrderService,
   ) {}
 
+  @Get('/prices/current')
+  @ApiOperation({
+    summary: 'Gets all crypto current prices',
+  })
+  @ApiResponse({
+      status: 200,
+      description: `Prices of the requested coins`,
+      type: CoinsPriceResponse
+    })
+  async getPrices(): Promise<CoinsPriceResponse> {
+    const result = await this.coinGeckoService.getCurrentCoinsPrice([
+      Coins.BITCOIN,
+      Coins.ETHEREUM,
+      Coins.DOGE,
+    ]);
+
+    if (isSuccess(result)) {
+      return result.success;
+    } else {
+      throw result.failure;
+    }
+  }
+
   @Post('/order')
   @ApiOperation({
     summary: 'Creates a buy order',
   })
   @ApiResponse({
-    status: 201,
-    description: `Order created successfully`,
-    type: OrderResponse,
-  })
+      status: 201,
+      description: `Order created successfully`,
+      type: OrderResponse
+    })
   @ApiResponse({
-    status: 400,
-    description: `Bad request`,
-  })
-  @ApiResponse({
-    status: 401,
-    description: `Unauthorized`,
-  })
-  @ApiResponse({
-    status: 412,
-    description: `Precondition failed (user does not have enough funds)`,
-  })
+      status: 400,
+      description: `Bad request`
+    })
+    @ApiResponse({
+      status: 401,
+      description: `Unauthorized`
+    })
+    @ApiResponse({
+      status: 412,
+      description: `Precondition failed (user does not have enough funds)`
+    })
   async createOrder(
     @Body() order: CreateOrderRequest,
     @JwtToken() token: JwtTokenResponse,
