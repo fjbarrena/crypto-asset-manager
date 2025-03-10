481f0ed40b6e362ce3af3e7711d5fbe93e7ae863
Paco Barrena <fbarrena@fermax.com>
2025-03-10 15:18:51 +0100

    chore: added missing validators

--- src/modules/cryptoassets/entities/order.entity.ts
+++ src/modules/cryptoassets/entities/order.entity.ts
@@ -2,6 +2,7 @@ import { BaseEntity } from 'src/entities/base.entity';
 import { Entity, Column, ManyToOne } from 'typeorm';
 import { Coins } from 'src/model/coins.enum';
 import { User } from 'src/modules/users/entities/user.entity';
+import { IsArray, IsCurrency, IsEnum, IsNumber, IsPositive } from 'class-validator';
 
 @Entity()
 export class Order extends BaseEntity {
@@ -10,26 +11,34 @@ export class Order extends BaseEntity {
     enum: Coins,
     enumName: 'Coins',
   })
+  @IsEnum(Coins)
   asset: Coins;
 
   @Column({
     type: 'integer',
     unsigned: true,
   })
+  @IsNumber()
+  @IsPositive()
   quantity: number;
 
   @Column({
     type: 'float',
     unsigned: true,
   })
+  @IsNumber()
+  @IsPositive()
   priceUSD: number;
 
   @Column({
     type: 'float',
     unsigned: true,
   })
+  @IsNumber()
+  @IsPositive()
   priceEUR: number;
 
   @ManyToOne(() => User, (user) => user.id)
+  @IsArray()
   buyer: User;
 }
--- src/modules/security/dtos/login.request.ts
+++ src/modules/security/dtos/login.request.ts
@@ -1,13 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 
 export class LoginRequest {
   @ApiProperty({
     description: 'Unique username',
   })
+  @IsString()
   username: string;
 
   @ApiProperty({
     description: 'Plain password',
   })
+  @IsString()
   plainPassword: string;
 }
