daa7e253620ffb6944a2bf8ec90c8f67f8ec6e5e
Francisco Javier Barrena <fjbarrena@gmail.com>
2025-03-11 00:42:52 +0100

    cicd: added lint to cicd

--- src/decorators/jwt-token.decorator.ts
+++ src/decorators/jwt-token.decorator.ts
@@ -2,6 +2,5 @@ import { createParamDecorator } from '@nestjs/common';
 import { JwtTokenResponse } from 'src/modules/security/dtos/jwt-token.response';
 
 export const JwtToken = createParamDecorator((data, req) => {
-  // eslint-disable-next-line
   return req.switchToHttp().getRequest().user as JwtTokenResponse;
 });
