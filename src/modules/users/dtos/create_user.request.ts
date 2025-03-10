732f572d3f0b587fe54380ebfc1a106cf694d82c
Paco Barrena <fbarrena@fermax.com>
2025-03-10 13:14:30 +0100

    feat: added validations

--- src/modules/users/dtos/create_user.request.ts
+++ src/modules/users/dtos/create_user.request.ts
@@ -1,24 +1,40 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Role } from '../enums/role.enum';
+import { IsAlphanumeric, IsEnum, IsNumber, IsPositive, Matches, MinLength } from 'class-validator';
 
 export class CreateUserRequest {
   @ApiProperty({
     description: 'Unique username',
   })
+  @IsAlphanumeric()
   username: string;
 
   @ApiProperty({
     description: 'Plain password (will be encrypted on the database)',
   })
+  @MinLength(10)
+  @Matches(
+    /((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
+    {
+      message: [
+        "Passwords will contain at least 1 upper case letter",
+        "Passwords will contain at least 1 lower case letter",
+        "Passwords will contain at least 1 number or special character",
+      ].join("\n")
+    }
+  )
   plainPassword: string;
 
   @ApiProperty({
     description: 'Role of the user',
   })
+  @IsEnum(Role)
   role: Role;
 
   @ApiProperty({
     description: 'Initial balance of the user',
   })
+  @IsNumber()
+  @IsPositive()
   initialBalance: number;
 }
--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -3,6 +3,7 @@ import { Entity, Column, OneToMany } from 'typeorm';
 import { Role } from '../enums/role.enum';
 import { Order } from 'src/modules/cryptoassets/entities/order.entity';
 import { Fiat } from 'src/model/fiat.enum';
+import { IsAlphanumeric, IsArray, IsEnum, IsNumber, Matches, MinLength } from 'class-validator';
 
 @Entity()
 export class User extends BaseEntity {
@@ -10,29 +11,45 @@ export class User extends BaseEntity {
     type: 'varchar',
     unique: true,
   })
+  @IsAlphanumeric()
   username: string;
 
   @Column({
     type: 'varchar',
   })
+  @MinLength(10)
+  @Matches(
+    /((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
+    {
+      message: [
+        "Passwords will contain at least 1 upper case letter",
+        "Passwords will contain at least 1 lower case letter",
+        "Passwords will contain at least 1 number or special character",
+      ].join("\n")
+    }
+  )
   encryptedPassword: string;
 
   @Column({
     enum: Role,
   })
+  @IsEnum(Role)
   role: Role;
 
   @Column({
     type: 'float',
     default: 0.0,
   })
+  @IsNumber()
   balance: number;
 
   @Column({
     enum: Fiat,
   })
+  @IsEnum(Fiat)
   balanceCurrency: Fiat;
 
   @OneToMany(() => Order, (order) => order.buyer)
+  @IsArray()
   orders: Order[];
 }
